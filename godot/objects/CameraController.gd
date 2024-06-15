142b69498b01d2e556e5217dd65c8ba3cf014f4a
Akshat Shukla <shuklaakshatascom@gmail.com>
2024-06-15 12:19:48 +0530

    Added mobile controls

--- godot/objects/CameraController.gd
+++ godot/objects/CameraController.gd
@@ -7,6 +7,7 @@ signal update_map(new_pos)
 @export var UPDATE_THRESHOLD: int = 64
 
 var last_pos: Vector2 = Vector2.ZERO
+var dragging: bool = false
 
 func _ready() -> void:
 	last_pos = self.get_target_position()
@@ -29,12 +30,25 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		match event.button_index:
 			MOUSE_BUTTON_WHEEL_UP:
+				if event.is_pressed():
+					return
 				# Zoom in
 				self.zoom += ZOOM_FACTOR
 			MOUSE_BUTTON_WHEEL_DOWN:
+				if event.is_pressed():
+					return
 				# Zoom Out
 				var new_zoom = self.zoom - ZOOM_FACTOR
 				if new_zoom.x <= 0 or new_zoom.y <= 0:
 					return
 				
 				self.zoom -= ZOOM_FACTOR
+			MOUSE_BUTTON_LEFT:
+				if event.is_pressed() and not dragging:
+					dragging = true
+				else:
+					dragging = false
+	
+	if event is InputEventMouseMotion:
+		if dragging:
+			self.global_position += event.relative * -1
